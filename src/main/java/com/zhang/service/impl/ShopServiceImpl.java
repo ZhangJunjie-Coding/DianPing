78a5982af4c632b64e3ee1b32cbff06c5a714a6a
张俊杰 <2298532487@qq.com>
2025-05-26 21:42:06 +0800

    first dianping

--- src/main/java/com/zhang/service/impl/ShopServiceImpl.java
+++ src/main/java/com/zhang/service/impl/ShopServiceImpl.java
@@ -11,6 +11,7 @@ import com.zhang.service.IShopService;
 import com.zhang.utils.RedisConstants;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.util.concurrent.TimeUnit;
@@ -38,13 +39,19 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
             return Result.fail("店铺不存在!");
         }
 
-        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shop),RedisConstants.CACHE_SHOP_TTL, TimeUnit.MINUTES);
+        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shop), RedisConstants.CACHE_SHOP_TTL, TimeUnit.MINUTES);
         return Result.ok(shop);
     }
 
     @Override
+    @Transactional
     public Result update(Shop shop) {
-
-        return null;
+        Long id = shop.getId();
+        if (id == null) {
+            return Result.fail("店铺id不能为空!");
+        }
+        updateById(shop);
+        stringRedisTemplate.delete(RedisConstants.CACHE_SHOP_KEY + shop.getId());
+        return Result.ok();
     }
 }
